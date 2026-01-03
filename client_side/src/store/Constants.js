a162c770e8abfad4220649675dcfc755b25bc6c4
ahmed8mansour <a.m424134567@gmail.com>
2026-01-03 21:14:37 +0200

    edit the urls

--- client_side/src/store/Constants.js
+++ client_side/src/store/Constants.js
@@ -1,1 +1,2 @@
-export const backendURL = "http://localhost:8000/"
+// export const backendURL = "http://localhost:8000/"
+export const backendURL = "https://full-stack-e-commerceproject-production.up.railway.app/"
